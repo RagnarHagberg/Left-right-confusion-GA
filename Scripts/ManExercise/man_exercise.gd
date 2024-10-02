65b1a22398659bb6b08447722d6509c440bd8039
AdamMarklund <adam.n.marklund@gmail.com>
2024-10-02 14:30:14 +0200

    add left right detection to ManExercise and grass animation

--- Scripts/ManExercise/man_exercise.gd
+++ Scripts/ManExercise/man_exercise.gd
@@ -1,11 +1,17 @@
 extends Node2D
+@onready var box: Area2D = $Man/Box
+@onready var man: Sprite2D = $Man
 
-@onready var man_node = get_node("Man")
-@onready var box_node = get_node("Box")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
-		man_node.rotation = randf_range(0,2*PI)
-	
+		man.rotation = randf_range(0,2*PI)
+		var box_angle = randf_range(0,2*PI)
+		var box_distance = randf_range(300,600)
+		box.position = Vector2(cos(box_angle), sin(box_angle)) * box_distance
+		if box_angle > PI/2 and box_angle < 3*PI/2:
+			print('Vänster')
+		else:
+			print('Höger')
 	
 	
